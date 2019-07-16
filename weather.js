c3ea98b311bff46a79bc433ec99db8a4c3bd59e7
Paul Galinsky <paul@designimage.eu>
2019-07-16 15:21:18 +0100

    Updated to https

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ class Weather {
 
 	// Fetch weather from API
 	async getWeather() {
-		const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&APPID=${this.apiKey}`);
+		const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&APPID=${this.apiKey}`);
 		const responseData = await response.json();
 		return responseData
 	}
